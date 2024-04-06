aabbef618f33fc8dba58582c01cc699269dc8b09
Owen Murphy <111613450+owentm@users.noreply.github.com>
2024-04-06 16:45:01 -0500

    finished algorithm, working on bot now

--- agent.py
+++ agent.py
@@ -1,11 +1,86 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as f
+import os
 import random
-import numpy as np
-from game import *
-from breaks import *
-from ball import *
-from paddle import *
 
-def main():
-    LOOPS = 1000
-    STEPS = 1000
+class NETWORK(nn.Module):
+        def __init__(self, n_obv, n_action):
+            super().__init__()
+            self.linear1 = nn.Linear(n_obv, 256)
+            self.linear2 = nn.Linear(n_action, 256)
+
+        def forward(self, train):
+            train = self.relu(self.linear1(train))
+            train = self.linear2(train)
+            return train
+
+class MODEL:
+        def __init__(self, model):
+            self.epsilon = 1.0
+            self.decay_rate = 0.005
+            self.learning_rate = 0.9
+            self.discount_rate = 0.8
+            self.prevAction = None
+            self.prevState = None
+            self.model = model
+            self.optimize = optim.Adam(model.parameters(), lr=self.learning_rate)
+            self.criterion = nn.MSELoss()
+
+
+
+        def chooseAction(self, state):
+            if random.random() < self.epsilon:
+                action = random.randint(0, 2)
+            else:
+                stateTensor = torch.tensor(state, dtype=torch.float)
+                qValues = self.model(stateTensor)
+                action = torch.argmax(qValues).item()
+            return action
+
+        def trainStep(self, state, action, reward, nextState, done):
+            state = torch.tensor(state, dtype=torch.float)
+            nextState = torch.tensor(nextState, dtype=torch.float)
+            action = torch.tensor(action, dtype=torch.long)
+            reward = torch.tensor(reward, dtype=torch.float)
+
+            if len(state.shape) == 1:
+                     state = torch.unsqueeze(state, 0)
+                     nextState = torch.unsqueeze(nextState, 0)
+                     action = torch.unsqueeze(nextState, 0)
+                     reward = torch.unsqueeze(nextState, 0)
+                     done = (done, )
+
+            prediction = self.model(state)
+            ob = prediction.clone()
+            for x in range(len(done)):
+                Q = reward[x]
+                if not done[x]:
+                    Q = reward[x] + self.discount_rate * torch.max(self.model(nextState[x]))
+
+                ob[x][torch.argmax(action[x]).item()] = Q
+
+            self.optimize.zero_grad()
+            lose = self.criterion(ob, prediction)
+            lose.backward()
+
+            self.optimize.step()
+
+
+
+class Agent:
+    def __init__(self):
+        self.game = 0
+        self.epsilon = 0
+        self.discount_rate = 0.9
+
+
+    def chooseAction(self, state):
+        if random.random() < self.epsilon:
+            action = random.randint(0, 2)
+        else:
+            stateTensor = torch.tensor(state, dtype=torch.float)
+            qValues = self.model(stateTensor)
+            action = torch.argmax(qValues).item()
+        return action
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import turtle as tr
 import torch
 import breaks
+from agent import Agent, MODEL, NETWORK
 from paddle import Mover
 from ball import Hitter
 from breaks import Wall
@@ -10,121 +11,150 @@ import time
 
 
 
-screen = tr.Screen()
+
+class game:
+    def __init__(self):
+
+        self.screen = tr.Screen()
 
 
 
 
 
-screen.setup(width=1200, height=600)
-screen.bgcolor('black')
-screen.tracer(0)
+        self.screen.setup(width=1200, height=600)
+        self.screen.bgcolor('black')
+        self.screen.tracer(0)
 
-UI = ui()
-UI.text()
+        self.UI = ui()
+        self.UI.text()
+        self.AI = Agent()
 
-score = Scoreboard(lives=5)
-
-
-player = Mover()
-objective = Hitter()
-bricks = Wall()
-
-inProgress = True
+        self.score = Scoreboard(lives=5)
 
 
-screen.listen()
-screen.onkey(key='Left', fun=player.moveLeft)
-screen.onkey(key='Right', fun=player.moveRight)
+        self.player = Mover()
+        self.objective = Hitter()
+        self.bricks = Wall()
 
-def hasHit():
-        global objective, inProgress
-
-        if objective.xcor() < -580 or objective.xcor() > 570:
-            objective.hit(xHit=True, yHit=False)
-            return
-
-        if objective.ycor() > 270:
-            objective.hit(xHit=False, yHit=True)
-            return
-
-        if objective.ycor() < -280:
-
-            objective.reset()
-            score.decreaseLives()
-            if score.lives == 0:
-                score.reset()
-                UI.gameOver(False)
-                inProgress = False
-                return
-
-            return
-
-def paddleHit():
-        global objective, player
-        playerX = player.xcor()
-        objectiveX = objective.xcor()
-
-        if objective.distance(player) < 110 and objective.ycor() < -250:
-            if playerX > 0:
-                if objectiveX > playerX:
-                    objective.hit(xHit=True, yHit=True)
-                    return
-                else:
-                    objective.hit(xHit=False, yHit=True)
-                    return
-
-            elif playerX < 0:
-                if objectiveX < playerX:
-                    objective.hit(xHit=True, yHit=True)
-                    return
-                else:
-                    objective.hit(xHit=False, yHit=True)
-                    return
+        self.inProgress = True
 
 
-            else:
-                if objectiveX > playerX:
-                    objective.hit(xHit=True, yHit=True)
-                    return
-                elif objectiveX < playerX:
-                    objective.hit(xHit=True, yHit=True)
-                    return
-                else:
-                    objective.hit(xHit=False, yHit=True)
-                    return
+        self.screen.listen()
+        self.screen.onkey(key='Left', fun=player.moveLeft)
+        self.screen.onkey(key='Right', fun=player.moveRight)
 
-def blockHit():
-        global objective, bricks
+    def reset(self):
+        self.score.reset()
+        self.player.reset()
+        self.objective.reset()
+        self.bricks.reset()
+        self.inProgress = True
 
-        for brick in bricks.bricks:
-            if objective.distance(brick) < 40:
-                score.increaseScore()
-                brick.quantity -= 1
-                if brick.quantity == 0:
-                    brick.clear()
-                    brick.goto(3000, 3000)
-                    bricks.bricks.remove(brick)
-                if objective.xcor() < brick.left_wall:
+    def playStep(self):
+        while self.inProgress:
+            self.screen.update()
+            time.sleep(.01)
+            state = {
+                'agentXCor': self.player.xcor(),
+                'objXCor': self.objective.xcor(),
+                'objYCor': self.objective.ycor(),
+                'brickQuan': len(self.bricks.bricks),
+                'brickLayout': [(brick.xcor(), brick.ycor()) for brick in self.bricks.bricks]
+            }
+
+            action = self.MODEL.chooseAction(state=state)
+
+            if action == 0:
+                self.player
+            elif action == 1:
+                self.player.moveLeft()
+            elif action == 2:
+                self.player.moveRight()
+
+            self.objective.fly()
+            self.blockHit()
+            self.paddleHit()
+            self.hasHit()
+            if len(bricks.bricks) == 0:
+                self.UI.gameOver(win=True)
+                break
+
+        def hasHit():
+                global objective, inProgress
+
+                if objective.xcor() < -580 or objective.xcor() > 570:
                     objective.hit(xHit=True, yHit=False)
-                elif objective.xcor() > brick.right_wall:
-                    objective.hit(xHit=True, yHit=False)
-                elif objective.ycor() < brick.bottom_wall:
+                    return
+
+                if objective.ycor() > 270:
                     objective.hit(xHit=False, yHit=True)
-                elif objective.ycor() > brick.upper_wall:
-                    objective.hit(xHit=False, yHit=True)
+                    return
+
+                if objective.ycor() < -280:
+
+                    self.objective.reset()
+                    self.score.decreaseLives()
+                    if self.score.lives == 0:
+                        self.score.reset()
+                        self.UI.gameOver(False)
+                        inProgress = False
+                        return
+
+                    return
+
+        def paddleHit():
+                global objective, player
+                playerX = player.xcor()
+                objectiveX = objective.xcor()
+
+                if objective.distance(player) < 110 and objective.ycor() < -250:
+                    if playerX > 0:
+                        if objectiveX > playerX:
+                            objective.hit(xHit=True, yHit=True)
+                            return
+                        else:
+                            objective.hit(xHit=False, yHit=True)
+                            return
+
+                    elif playerX < 0:
+                        if objectiveX < playerX:
+                            objective.hit(xHit=True, yHit=True)
+                            return
+                        else:
+                            objective.hit(xHit=False, yHit=True)
+                            return
 
 
-while inProgress:
-    screen.update()
-    time.sleep(.01)
-    objective.fly()
-    blockHit()
-    paddleHit()
-    hasHit()
-    if len(bricks.bricks) == 0:
-        UI.gameOver(win=True)
-        break
+                    else:
+                        if objectiveX > playerX:
+                            objective.hit(xHit=True, yHit=True)
+                            return
+                        elif objectiveX < playerX:
+                            objective.hit(xHit=True, yHit=True)
+                            return
+                        else:
+                            objective.hit(xHit=False, yHit=True)
+                            return
+
+        def blockHit():
+                global objective, bricks
+
+                for brick in bricks.bricks:
+                    if objective.distance(brick) < 40:
+                        self.score.increaseScore()
+                        brick.quantity -= 1
+                        if brick.quantity == 0:
+                            brick.clear()
+                            brick.goto(3000, 3000)
+                            bricks.bricks.remove(brick)
+                        if objective.xcor() < brick.left_wall:
+                            objective.hit(xHit=True, yHit=False)
+                        elif objective.xcor() > brick.right_wall:
+                            objective.hit(xHit=True, yHit=False)
+                        elif objective.ycor() < brick.bottom_wall:
+                            objective.hit(xHit=False, yHit=True)
+                        elif objective.ycor() > brick.upper_wall:
+                            objective.hit(xHit=False, yHit=True)
 
 
 
@@ -137,5 +167,9 @@ while inProgress:
 
 
 
+
+
+
+play = game()
+game.playStep()
 tr.mainloop()
-
